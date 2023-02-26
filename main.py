ef3aec4ea82b835798fb6b21bd5e9cc5e1633f43
LionSandwhich <mattjanas@gmail.com>
2023-02-26 10:06:51 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+
+def load_window_buttons():
+
+
+def setup_window():
+    # create window of tkinter
+    window = tk.Tk()
+    greeting = tk.Label(text="LionRacing SimTool")
+    greeting.pack()
+    window.mainloop()
+
+if __name__ == '__main__':
+    setup_window()
+
+
